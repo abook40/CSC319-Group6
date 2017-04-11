90d3f727a046358dae9cc0e58f1aa243c5ece5ca
APgangsater <APgangsater@hotmail.com>
2017-04-11 21:42:47 +0700

    Update Main.java

--- HashTable , WordEntry, and Main/Main.java
+++ HashTable , WordEntry, and Main/Main.java
@@ -1,9 +1,23 @@
-public class Main{
-  private String line;
-  private int score;
-  privte HashTable table(int size);
-  private String message;
-  
-  public Main()
-  {}
-}
+  import java.io.BufferedReader;
+  import java.io.File;
+  import java.io.FileReader;
+  import java.io.IOException;
+  public class Main{
+    public static void main(String args[])
+    {
+    private String line;
+    private int score;
+    privte HashTable table(int size);
+    private String message;
+    public Main()
+    {
+   HarshTable ht = new HarshTable(2000);
+   
+    }
+    public createHarshTable(int size)
+    {
+    
+    }
+    }
+    }
+  }
